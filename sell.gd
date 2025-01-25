b740ebbda51a26e3fc7070a36bdf0fe2adc3cb26
Temp <temp.flavr@gmail.com>
2025-01-25 12:54:44 +0900

    1.05x magnification makes a funny bug, so we'll go with 1.1. Also cleaned up

--- sell.gd
+++ sell.gd
@@ -4,8 +4,10 @@ extends Control
 @onready var price_label : Label = %PriceLabel
 @onready var box : PanelContainer = %SellContainer
 
-var no_sale_text : String = "--"
 var holding_sellable : bool = false
+
+const c_normal_size : float = 1.0
+const c_wombo_size : float = 1.1
 
 func _ready() -> void:
 	GameState.drag_object_changed.connect(update_price)
@@ -15,8 +17,7 @@ func update_price(drag_obj: Node):
 	if drag_obj == null or drag_obj.from_shop:
 		price_label.text = "--"
 		holding_sellable = false
-		price_label.text = no_sale_text
-		set_box_scale(1)
+		set_box_scale(c_normal_size)
 		return
 	price_label.text = str(drag_obj.sell_price)
 	holding_sellable = true
@@ -25,13 +26,13 @@ func update_price(drag_obj: Node):
 func _on_panel_container_mouse_entered() -> void:
 	GameState.drag_sell_button = true
 	if holding_sellable:
-		set_box_scale(1.05)
+		set_box_scale(c_wombo_size)
 
 
 func _on_panel_container_mouse_exited() -> void:
 	GameState.drag_sell_button = false
 	if holding_sellable:
-		set_box_scale(1)
+		set_box_scale(c_normal_size)
 	
 func set_box_scale(val: float):
 	if box.scale.x != val:
